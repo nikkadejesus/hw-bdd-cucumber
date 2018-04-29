2fef2493ca484c92db627b5915796a00bb71c388
cylwyn-creer <cylwyncreer@gmail.com>
2018-04-29 17:56:36 +0800

    starting part 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,9 +1,14 @@
 # Add a declarative step here for populating the DB with movies.
-
+# +=|\{[
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    new_movie = Movie.new
+    new_movie.title = movie['title']
+    new_movie.rating = movie['rating']
+    new_movie.release_date = movie['release_date']
+    new_movie.save
   end
   fail "Unimplemented"
 end
